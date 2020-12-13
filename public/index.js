078b6be31293f34700fcfc3f0872b6db6cf18b52
Rutaj Dash <33367546+rutajdash@users.noreply.github.com>
2020-12-13 14:22:17 +0530

    Loading + Basic Site Ready (#1)
    * update loading screen styling
    
    * remote config and redirect setup

---
+++ public/index.js
@@ -0,0 +1,25 @@
+const remoteConfig = firebase.remoteConfig();
+remoteConfig.settings.minimumFetchIntervalMillis = 1200000;
+remoteConfig.defaultConfig = {
+	orientation_status: 1,
+};
+remoteConfig
+	.fetchAndActivate()
+	.then(() => {
+		const val = remoteConfig.getValue("orientation_status").asNumber();
+		switch (val) {
+			case 0:
+				if (window.location.pathname != "/comingSoon") window.location = "/comingSoon";
+				break;
+			case 2:
+				if (window.location.pathname != "/thankYou") window.location = "/thankYou";
+				break;
+			default:
+				if (window.location.pathname != "/orientation") window.location = "/orientation";
+				break;
+		}
+	})
+	.catch((err) => {
+		console.error(err);
+		if (window.location.pathname != "/orientation") window.location = "/orientation";
+	});
